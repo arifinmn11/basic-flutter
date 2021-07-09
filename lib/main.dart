e443f6b8aa6a6c8aa9f34acc5c240f9cd40b6010
arifinmn11 <nurarifinmuhammad@gmail.com>
2021-07-09 17:18:35 +0700

    Basic text widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.red,
-        ),
         home: Scaffold(
           appBar: AppBar(
-            title: Text("Aplikasi Hello WOrld"),
+            title: Text("Aplikasi Hello World"),
           ),
           body: Center(
             child: Container(
               color: Colors.lightBlue,
               width: 150,
               height: 50,
-              child: Text("Hello world!"),
+              child: Text(
+                "Saya sedang melatih kemmapuan saya menggunakan flutter!!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
             ),
           ),
         ));
