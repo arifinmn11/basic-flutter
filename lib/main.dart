d536e6ade791b50007d85295e4c905abda2d8d61
arifinmn11 <nurarifinmuhammad@gmail.com>
2021-07-09 17:28:10 +0700

    add row and column

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,33 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Aplikasi Hello World"),
-          ),
-          body: Center(
-            child: Container(
-              color: Colors.lightBlue,
-              width: 150,
-              height: 50,
-              child: Text(
-                "Saya sedang melatih kemmapuan saya menggunakan flutter!!",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20),
-              ),
-            ),
-          ),
-        ));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Row and Coloumn")
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Text 1"),
+            Text("Text 2"),
+            Text("Text 3"),
+            Row(
+              children: <Widget>[
+                Text("Text 5"),
+                Text("Text 6"),
+              ],
+            )
+          ]
+          ,
+        ),
+      ),
+    );
   }
 }
