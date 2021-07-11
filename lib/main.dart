870f3488f4f116546af3ef5a1b189b983bb89506
arifinmn11 <nurarifinmuhammad@gmail.com>
2021-07-12 03:05:22 +0700

    example draggable send value color

--- lib/main.dart
+++ lib/main.dart
@@ -5,48 +5,116 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color color1 = Colors.red;
+  Color color3 = Colors.orange;
+  Color color2 = Colors.grey;
+  bool isAccepted = false;
+
+  Color targetColor = Colors.transparent;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Example Stack and Align'),
+          title: Text('Example Dragable'),
         ),
-        body: Center(
-          child: Row(
-            children: <Widget>[
-              Spacer(
-                flex: 1,
-              ),
-              Container(
-                height: 80,
-                width: 80,
-                color: Colors.red,
-              ),
-              Spacer(
-                flex: 2,
-              ),
-              Container(
-                height: 80,
-                width: 80,
-                color: Colors.green,
-              ),
-              Spacer(
-                flex: 2,
-              ),
-              Container(
-                height: 80,
-                width: 80,
-                color: Colors.blue,
-              ),
-              Spacer(
-                flex: 1,
-              ),
-            ],
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Draggable<Color>(
+                  data: color1,
+                  feedback: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                        color: color1.withOpacity(0.7), shape: StadiumBorder()),
+                  ),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color1,
+                      shape: StadiumBorder(),
+                      elevation: 2,
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color2.withOpacity(0.2),
+                      shape: StadiumBorder(),
+                    ),
+                  ),
+                ),
+                Draggable<Color>(
+                  data: color3,
+                  feedback: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                        color: color3.withOpacity(0.7), shape: StadiumBorder()),
+                  ),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color3,
+                      shape: StadiumBorder(),
+                      elevation: 2,
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color2.withOpacity(0.2),
+                      shape: StadiumBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            DragTarget<Color>(
+              onWillAccept: (value) => true,
+              onAccept: (value) {
+                isAccepted = true;
+                targetColor = value;
+              },
+              builder: (context, candidates, rejected) {
+                return (isAccepted)
+                    ? SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Material(
+                          color: targetColor,
+                          shape: StadiumBorder(),
+                        ),
+                      )
+                    : SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Material(
+                          color: color2.withOpacity(0.5),
+                          shape: StadiumBorder(),
+                          elevation: 2,
+                        ),
+                      );
+              },
+            )
+          ],
         ),
       ),
     );
