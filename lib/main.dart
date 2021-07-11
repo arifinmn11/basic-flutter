939840a446fce24135719256dc3601c03bb42009
arifinmn11 <nurarifinmuhammad@gmail.com>
2021-07-12 01:44:42 +0700

    example spacer widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,18 +16,36 @@ class MyApp extends StatelessWidget {
           title: Text('Example Stack and Align'),
         ),
         body: Center(
-          child: Container(
-            color: Colors.blue,
-            width: 200,
-            height: 200,
-            padding: EdgeInsets.all(3),
-            child: Image(
-              // image: NetworkImage(
-              //     'https://i.pinimg.com/originals/75/a2/16/75a21680e7a1b930648a367e693f6d80.jpg'),
-              image: AssetImage("images/1.png"),
-              fit: BoxFit.contain,
-              repeat: ImageRepeat.repeat,
-            ),
+          child: Row(
+            children: <Widget>[
+              Spacer(
+                flex: 1,
+              ),
+              Container(
+                height: 80,
+                width: 80,
+                color: Colors.red,
+              ),
+              Spacer(
+                flex: 2,
+              ),
+              Container(
+                height: 80,
+                width: 80,
+                color: Colors.green,
+              ),
+              Spacer(
+                flex: 2,
+              ),
+              Container(
+                height: 80,
+                width: 80,
+                color: Colors.blue,
+              ),
+              Spacer(
+                flex: 1,
+              ),
+            ],
           ),
         ),
       ),
